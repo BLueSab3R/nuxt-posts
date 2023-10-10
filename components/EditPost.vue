918384793255412fa290ed0522714a16c3d9a9eb
BLueSab3R <100284691+BLueSab3R@users.noreply.github.com>
2023-10-10 12:13:57 +0300

    remove useless folder and fix EditPost

--- components/EditPost.vue
+++ components/EditPost.vue
@@ -1,37 +1,70 @@
 <template>
-  <div @click="handleBackgroundClick"
-    class="fixed flex flex-col items-center justify-center max-h-screen inset-0 overflow-y-auto bg-black bg-opacity-50">
-    <div class="bg-white flex flex-col text-black p-40 rounded-lg relative">
-
-      <button @click="$emit('closeEdit')"
-        class="absolute top-2 right-2 rounded-md border-red-400 bg-red-400 border-solid text-xl hover:bg-red-200 border-2">
+  <div
+    @click="handleBackgroundClick"
+    class="fixed flex flex-col items-center justify-center max-h-screen inset-0 overflow-y-auto bg-black bg-opacity-50"
+  >
+    <div class="bg-white flex flex-col text-black p-20 rounded-lg relative">
+      <button
+        @click="$emit('closeEdit')"
+        class="absolute top-2 right-2 rounded-md border-red-400 bg-red-400 border-solid text-xl hover:bg-red-200 border-2"
+      >
         Close
       </button>
 
       <ul class="text-xl">
-        <li>
-          <label class="mr-2">Username:</label>
-          <input type="text" v-model="selectedPost.username" placeholder="new username" />
+        <li class="">
+          <label class="mr-2" for="selectedUsername">Username:</label>
+          <input
+            id="selectedUsername"
+            type="text"
+            v-model="formData.username"
+            placeholder="new username"
+          />
         </li>
         <li>
-          <label class="mr-2">Name:</label>
-          <input type="text" v-model="selectedPost.name" placeholder="new name" />
+          <label class="mr-2" for="selectedName">Name:</label>
+          <input
+            id="selectedName"
+            type="text"
+            v-model="formData.name"
+            placeholder="new name"
+          />
         </li>
         <li>
-          <label class="mr-2">Email:</label>
-          <input type="text" v-model="selectedPost.email" placeholder="new email" />
+          <label class="mr-2" for="selectedEmail">Email:</label>
+          <input
+            id="selectedEmail"
+            type="text"
+            v-model="formData.email"
+            placeholder="new email"
+          />
         </li>
         <li>
-          <label class="mr-2">Website:</label>
-          <input type="text" v-model="selectedPost.website" placeholder="new website" />
+          <label class="mr-2" for="selectedWebsite">Website:</label>
+          <input
+            id="selectedWebsite"
+            type="text"
+            v-model="formData.website"
+            placeholder="new website"
+          />
         </li>
         <li>
-          <label class="mr-2">City:</label>
-          <input type="text" v-model="selectedPost.city" placeholder="new city" />
+          <label class="mr-2" for="selectedCity">City:</label>
+          <input
+            id="selectedCity"
+            type="text"
+            v-model="formData.city"
+            placeholder="new city"
+          />
         </li>
         <li>
-          <label class="mr-2">Phone:</label>
-          <input type="text" v-model="selectedPost.phone" placeholder="new phone" />
+          <label class="mr-2" for="selectedPhone">Phone:</label>
+          <input
+            id="selectedPhone"
+            type="text"
+            v-model="formData.phone"
+            placeholder="new phone"
+          />
         </li>
       </ul>
       <button class="flex mt-20 justify-center" @click="updatePost">Confirm</button>
@@ -41,27 +74,42 @@
 
 <script setup>
 const client = useSupabaseClient();
-const emit = defineEmits(['closeEdit', 'updatePosts']);
-const props = defineProps(['selectedPost']);
+const emit = defineEmits(["closeEdit", "updatePosts"]);
+const props = defineProps(["selectedPost"]);
 const handleBackgroundClick = (event) => {
   if (event.target === event.currentTarget) {
-    emit('closeEdit');
+    emit("closeEdit");
   }
 };
 
+const formData = ref({
+  username: props.selectedPost.username,
+  name: props.selectedPost.name,
+  email: props.selectedPost.email,
+  website: props.selectedPost.website,
+  city: props.selectedPost.city,
+  phone: props.selectedPost.phone,
+});
+
 const updatePost = async () => {
-  const updatedData = {
-    username: props.selectedPost.username || null,
-    name: props.selectedPost.name || null,
-    email: props.selectedPost.email || null,
-    city: props.selectedPost.city || null,
-  };
-  const { data, error } = await client
-    .from('posts')
-    .update(updatedData)
-    .eq('id', props.selectedPost.id);
+  if (
+    !formData.value.username ||
+    formData.value.name ||
+    formData.value.email ||
+    formData.value.city ||
+    formData.value.phone
+  ) {
+    alert("Будь ласка, заповніть всі поля.");
+    return;
+  }
+  const { error } = await client
+    .from("posts")
+    .update(formData.value)
+    .eq("id", props.selectedPost.id)
+    .select();
   if (!error) {
-    emit('updatePosts', 'closeEdit');
+    emit("updatePosts");
+    emit("closeEdit");
   } else {
     console.error(error);
   }
